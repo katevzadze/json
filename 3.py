e8ac053bd09799eafbd082b274ec4f6b498e714a
katevzadze <143904679+katevzadze@users.noreply.github.com>
2024-04-03 17:52:44 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 import csv
 import json
-from datetime import date
+from datetime import datetime
 import re
 
 
@@ -11,17 +11,25 @@ class Client:
         self.birth_date = birth_date
         self.bonuses = bonuses
 
-    def validate(self):
+    def validate_name(self):
         if not re.match(r'^[А-Яа-яЁё]+$', self.name):
             return False
+        else:
+            return True
+    def validate_surname(self):
         if not re.match(r'^[А-Яа-яЁё]+$', self.surname):
             return False
+        else:
+            return True
+    def validate_birth_date(self):
         try:
-            birth_date = date.fromisoformat(self.birth_date)
-            if birth_date > date.today() or birth_date < date(1950, 1, 1):
+           birth_date = datetime.strptime(self.birth_date, "%d.%m.%Y")
+           if birth_date > datetime.now() or birth_date < datetime(1950, 1, 1):
                 return False
         except ValueError:
             return False
+        return True
+    def validate_bonuses(self):
         try:
             bonuses = int(self.bonuses)
             if bonuses < 0 or bonuses > 10000000:
@@ -41,14 +49,14 @@ with open('clients.csv', 'r') as f:
     for row in reader:
         name, surname, birth_date, bonuses = row
         client = Client(name, surname, birth_date, bonuses)
-        if client.validate():
+        if client.validate_name() and client.validate_surname() and client.validate_birth_date() and client.validate_bonuses():
             processed += 1
             clients.append(client)
         else:
             skipped += 1
 
 with open('clients.json', 'w') as f:
-    json.dump([c.__dict__ for c in clients], f, ensure_ascii=False)
+    json.dump([c.__dict__ for c in clients], f, ensure_ascii=False, indent=2)
 
 print(f'Обработано записей: {processed}')
 print(f'Пропущено записей: {skipped}')
